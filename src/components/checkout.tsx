09a145afc51398b707ea500884e1d8462a4e1976
Your Name <you@example.com>
2024-10-28 16:02:19 -0300

    quase fiz mp

--- src/components/checkout.tsx
+++ src/components/checkout.tsx
@@ -17,13 +17,14 @@ import {
   RadioGroup,
   IconButton,
   CircularProgress,
+  TextField,
 } from "@mui/material";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
-import { initMercadoPago } from "@mercadopago/sdk-js";
+import { initMercadoPago, CardForm } from "@mercadopago/sdk-js";
 
-const MercadoPago = require("@mercadopago/sdk-js");
-MercadoPago.initMercadoPago(process.env.REACT_APP_MERCADO_PAGO_PUBLIC_KEY!);
+// Inicializa o SDK do Mercado Pago
+initMercadoPago(process.env.REACT_APP_MERCADO_PAGO_PUBLIC_KEY!);
 
 interface CartItem {
   id: string;
@@ -38,8 +39,8 @@ const Checkout: React.FC = () => {
   const [shippingCost, setShippingCost] = useState<number>(0);
   const [totalPrice, setTotalPrice] = useState<number>(0);
   const [isLoading, setIsLoading] = useState(false);
-  const navigate = useNavigate();
   const [paymentMethod, setPaymentMethod] = useState<string>("PIX");
+  const navigate = useNavigate();
 
   useEffect(() => {
     const storedCart = localStorage.getItem("cart");
@@ -61,7 +62,9 @@ const Checkout: React.FC = () => {
     setTotalPrice(total);
   }, [cart, shippingCost]);
 
-  const handleCheckout = async () => {
+  const handleCheckout = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
     const token = localStorage.getItem("token");
     if (!token) {
       alert("Você precisa estar logado para finalizar a compra.");
@@ -71,14 +74,52 @@ const Checkout: React.FC = () => {
 
     setIsLoading(true);
 
+    const cardFormData = {
+      cardholderName: (
+        document.getElementById("cardholderName") as HTMLInputElement
+      ).value,
+      cardNumber: (document.getElementById("cardNumber") as HTMLInputElement)
+        .value,
+      expirationDate: (
+        document.getElementById("expirationDate") as HTMLInputElement
+      ).value,
+      securityCode: (
+        document.getElementById("securityCode") as HTMLInputElement
+      ).value,
+      installments: 1, // ou o número de parcelas escolhido
+      identificationType: "CPF", // ou outro tipo de identificação
+      identificationNumber: "12345678909",
+    };
+
     try {
+      // Gera o token usando o SDK do Mercado Pago
+      const cardForm = new CardForm({
+        amount: totalPrice.toString(),
+        form: {
+          id: "form-checkout",
+          cardholderName: { id: "cardholderName" },
+          cardNumber: { id: "cardNumber" },
+          expirationDate: { id: "expirationDate" },
+          securityCode: { id: "securityCode" },
+          installments: { id: "installments" },
+          identificationType: { id: "identificationType" },
+          identificationNumber: { id: "identificationNumber" },
+        },
+      });
+
+      const { token: cardToken } = await cardForm.createCardToken();
+
       const response = await axios.post(
-        "https://ecommerce-fagundes-13c7f6f3f0d3.herokuapp.com/payment/create-payment",
+        "https://ecommerce-fagundes-13c7f6f3f0d3.herokuapp.com/payment/create-transparent",
         {
-          products: cart,
-          paymentMethod, // Envia o método de pagamento escolhido
-          shippingCost,
+          token: cardToken,
+          transactionAmount: totalPrice,
+          description: "Compra de produtos",
+          installments: 1,
+          paymentMethodId: paymentMethod,
           email: "usuario@teste.com",
+          shippingCost,
+          products: cart,
         },
         {
           headers: {
@@ -87,37 +128,28 @@ const Checkout: React.FC = () => {
         }
       );
 
-      const paymentUrl = response.data.init_point;
-      window.location.href = paymentUrl; // Redireciona o usuário para o checkout do Mercado Pago
-    } catch (err) {
-      console.error("Erro ao finalizar o pedido:", err);
-      alert("Erro ao finalizar o pedido.");
+      if (response.data.status === "approved") {
+        navigate("/sucesso");
+      } else {
+        alert("Pagamento pendente ou falhou. Verifique sua transação.");
+      }
+    } catch (error) {
+      console.error("Erro ao processar pagamento:", error);
+      alert("Erro ao finalizar o pagamento.");
     } finally {
       setIsLoading(false);
     }
   };
 
-  const handleContinueShopping = () => {
-    navigate("/");
-  };
-
   return (
-    <Box
-      sx={{
-        padding: 3,
-        backgroundColor: "#f5f5f5",
-        minHeight: "100vh",
-      }}
-    >
+    <Box sx={{ padding: 3, backgroundColor: "#f5f5f5", minHeight: "100vh" }}>
       <IconButton
-        onClick={handleContinueShopping}
+        onClick={() => navigate("/")}
         sx={{
           color: "#313926",
           border: "1px solid #313926",
           marginBottom: "16px",
-          "&:hover": {
-            backgroundColor: "#e0e0e0",
-          },
+          "&:hover": { backgroundColor: "#e0e0e0" },
         }}
       >
         <ArrowBackIcon />
@@ -133,185 +165,133 @@ const Checkout: React.FC = () => {
         Resumo do Pedido
       </Typography>
 
-      <Grid container spacing={3}>
-        <Grid item xs={12} md={6}>
-          <Paper
-            elevation={3}
-            sx={{
-              padding: 2,
-              border: "1px solid #E6E3DB",
-              borderRadius: "8px",
-            }}
-          >
-            <Typography variant="h6" sx={{ fontWeight: "bold", mb: 2 }}>
-              Forma de Pagamento:
-            </Typography>
-            <RadioGroup
-              value={paymentMethod}
-              onChange={(e) => setPaymentMethod(e.target.value)}
-            >
-              <FormControlLabel
-                value="PIX"
-                control={
-                  <Radio
-                    sx={{
-                      color: "#313926",
-                      "&.Mui-checked": { color: "#313926" },
-                    }}
-                  />
-                }
-                label={
-                  <Box display="flex" alignItems="center" gap={1}>
-                    <img
-                      src="/icons8-pix.svg"
-                      alt="PIX Logo"
-                      style={{ width: 56, height: 56 }}
-                    />
-                    <Typography>PIX</Typography>
-                  </Box>
-                }
-              />
-
-              <FormControlLabel
-                value="Boleto Bancário"
-                control={
-                  <Radio
-                    sx={{
-                      color: "#313926",
-                      "&.Mui-checked": { color: "#313926" },
-                    }}
-                  />
-                }
-                label={
-                  <Box display="flex" alignItems="center" gap={1}>
-                    <img
-                      src="/icons8-boleto-64.png"
-                      alt="Boleto Logo"
-                      style={{ width: 56, height: 56 }}
-                    />
-                    <Typography>Boleto Bancário</Typography>
-                  </Box>
-                }
-              />
-
-              <FormControlLabel
-                value="Cartão de Crédito"
-                control={
-                  <Radio
-                    sx={{
-                      color: "#313926",
-                      "&.Mui-checked": { color: "#313926" },
-                    }}
-                  />
-                }
-                label={
-                  <Box display="flex" alignItems="center" gap={1}>
-                    <img
-                      src="/icons8-mastercard-credit-card-80.png"
-                      alt="Cartão de Crédito Logo"
-                      style={{ width: 56, height: 56 }}
-                    />
-                    <Typography>Cartão de Crédito</Typography>
-                  </Box>
-                }
-              />
-            </RadioGroup>
-          </Paper>
-        </Grid>
-
-        <Grid item xs={12} md={6}>
-          <Paper
-            elevation={3}
-            sx={{
-              padding: 2,
-              border: "1px solid #E6E3DB",
-              borderRadius: "8px",
-            }}
-          >
-            <Typography variant="h6" sx={{ fontWeight: "bold", mb: 2 }}>
-              Resumo do Pedido
-            </Typography>
-            <List>
-              {cart.map((item) => (
-                <div key={item.id}>
-                  <ListItem
-                    sx={{
-                      display: "flex",
-                      justifyContent: "space-between",
-                      alignItems: "center",
-                    }}
-                  >
-                    <Avatar
-                      variant="square"
-                      src={item.image}
-                      alt={item.name}
-                      sx={{ width: 60, height: 60, marginRight: 2 }}
-                    />
-                    <ListItemText
-                      primary={
-                        <Typography
-                          variant="h6"
-                          sx={{ fontWeight: "bold", color: "#313926" }}
-                        >
-                          {item.name}
-                        </Typography>
-                      }
-                      secondary={
-                        <Typography
-                          variant="body2"
-                          color="textSecondary"
-                          sx={{ fontSize: "0.9rem" }}
-                        >
-                          Quantidade: {item.quantity} | Preço: R${" "}
-                          {item.price.toFixed(2)}
-                        </Typography>
-                      }
-                    />
-                  </ListItem>
-                  <Divider />
-                </div>
-              ))}
-            </List>
-            <Typography
-              variant="h6"
-              sx={{ fontWeight: "bold", mt: 2, textAlign: "right" }}
-            >
-              Total dos Produtos: R$ {(totalPrice - shippingCost).toFixed(2)}
-            </Typography>
-            <Typography
-              variant="h6"
-              sx={{ fontWeight: "bold", mt: 1, textAlign: "right" }}
-            >
-              Frete: R$ {shippingCost.toFixed(2)}
-            </Typography>
-            <Typography
-              variant="h6"
-              sx={{ fontWeight: "bold", mt: 1, textAlign: "right" }}
-            >
-              Total Geral: R$ {totalPrice.toFixed(2)}
-            </Typography>
-            <Button
-              variant="contained"
-              color="primary"
-              onClick={handleCheckout}
-              fullWidth
+      <form id="form-checkout" onSubmit={handleCheckout}>
+        <Grid container spacing={3}>
+          <Grid item xs={12} md={6}>
+            <Paper
+              elevation={3}
               sx={{
-                mt: 3,
-                backgroundColor: "#313926",
-                "&:hover": { backgroundColor: "#1d2721" },
-                padding: "10px 0",
-                fontSize: "1rem",
-                fontWeight: "bold",
-                position: "relative",
+                padding: 2,
+                border: "1px solid #E6E3DB",
+                borderRadius: "8px",
               }}
-              disabled={isLoading}
-              startIcon={isLoading ? <CircularProgress size={24} /> : null}
             >
-              {isLoading ? "Processando..." : "Finalizar Pedido"}
-            </Button>
-          </Paper>
+              <Typography variant="h6" sx={{ fontWeight: "bold", mb: 2 }}>
+                Dados do Cartão:
+              </Typography>
+              <TextField
+                id="cardholderName"
+                label="Nome do Titular"
+                fullWidth
+                margin="normal"
+                required
+              />
+              <TextField
+                id="cardNumber"
+                label="Número do Cartão"
+                fullWidth
+                margin="normal"
+                required
+              />
+              <TextField
+                id="expirationDate"
+                label="Data de Validade (MM/YY)"
+                fullWidth
+                margin="normal"
+                required
+              />
+              <TextField
+                id="securityCode"
+                label="Código de Segurança"
+                fullWidth
+                margin="normal"
+                required
+              />
+            </Paper>
+          </Grid>
+
+          <Grid item xs={12} md={6}>
+            <Paper
+              elevation={3}
+              sx={{
+                padding: 2,
+                border: "1px solid #E6E3DB",
+                borderRadius: "8px",
+              }}
+            >
+              <Typography variant="h6" sx={{ fontWeight: "bold", mb: 2 }}>
+                Resumo do Pedido
+              </Typography>
+              <List>
+                {cart.map((item) => (
+                  <div key={item.id}>
+                    <ListItem
+                      sx={{
+                        display: "flex",
+                        justifyContent: "space-between",
+                        alignItems: "center",
+                      }}
+                    >
+                      <Avatar
+                        variant="square"
+                        src={item.image}
+                        alt={item.name}
+                        sx={{ width: 60, height: 60, marginRight: 2 }}
+                      />
+                      <ListItemText
+                        primary={
+                          <Typography
+                            variant="h6"
+                            sx={{ fontWeight: "bold", color: "#313926" }}
+                          >
+                            {item.name}
+                          </Typography>
+                        }
+                        secondary={
+                          <Typography
+                            variant="body2"
+                            color="textSecondary"
+                            sx={{ fontSize: "0.9rem" }}
+                          >
+                            Quantidade: {item.quantity} | Preço: R${" "}
+                            {item.price.toFixed(2)}
+                          </Typography>
+                        }
+                      />
+                    </ListItem>
+                    <Divider />
+                  </div>
+                ))}
+              </List>
+              <Typography
+                variant="h6"
+                sx={{ fontWeight: "bold", mt: 2, textAlign: "right" }}
+              >
+                Total Geral: R$ {totalPrice.toFixed(2)}
+              </Typography>
+              <Button
+                variant="contained"
+                color="primary"
+                type="submit"
+                fullWidth
+                sx={{
+                  mt: 3,
+                  backgroundColor: "#313926",
+                  "&:hover": { backgroundColor: "#1d2721" },
+                  padding: "10px 0",
+                  fontSize: "1rem",
+                  fontWeight: "bold",
+                  position: "relative",
+                }}
+                disabled={isLoading}
+                startIcon={isLoading ? <CircularProgress size={24} /> : null}
+              >
+                {isLoading ? "Processando..." : "Finalizar Pedido"}
+              </Button>
+            </Paper>
+          </Grid>
         </Grid>
-      </Grid>
+      </form>
     </Box>
   );
 };
