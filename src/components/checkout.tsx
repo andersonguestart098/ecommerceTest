fef49f8db75b1979d1ab37053244cd4c83911ed0
Your Name <you@example.com>
2024-10-30 17:10:20 -0300

    acrescentando pix

--- src/components/checkout.tsx
+++ src/components/checkout.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
@@ -6,170 +6,138 @@ const Checkout: React.FC = () => {
   const navigate = useNavigate();
   const [isMpReady, setIsMpReady] = useState(false);
   const [sdkLoaded, setSdkLoaded] = useState(false);
-  const [selectedPaymentMethod, setSelectedPaymentMethod] = useState("card");
+  const [mpInstance, setMpInstance] = useState<any>(null);
+  const [selectedPaymentMethod, setSelectedPaymentMethod] = useState<
+    string | null
+  >(null);
   const publicKey = process.env.REACT_APP_MERCADO_PAGO_PUBLIC_KEY;
-  const [deviceId, setDeviceId] = useState<string | null>(null);
   const [checkoutData, setCheckoutData] = useState<any>({});
-  const [installmentOptions, setInstallmentOptions] = useState<number[]>([]);
-  const [selectedInstallment, setSelectedInstallment] = useState(1);
-  const [userId, setUserId] = useState<string | null>(null);
-  let mpInstance: any; // Variável global para a instância do MercadoPago
+
+  const formRef = useRef<HTMLFormElement | null>(null);
 
   useEffect(() => {
-    const user = localStorage.getItem("user");
-    if (user) {
-      const parsedUser = JSON.parse(user);
-      setUserId(parsedUser.id);
-    }
-
-    const data = JSON.parse(localStorage.getItem("checkoutData") || "{}");
-    setCheckoutData(data);
-
-    if (!publicKey) {
-      console.error("Chave pública do Mercado Pago não encontrada!");
-      return;
-    }
-
-    const loadMercadoPagoSdk = () => {
-      return new Promise<void>((resolve, reject) => {
-        if (window.MercadoPago) {
-          console.log("SDK do Mercado Pago já carregado");
+    const loadMercadoPagoSdk = async () => {
+      if (window.MercadoPago) {
+        setSdkLoaded(true);
+        setMpInstance(new window.MercadoPago(publicKey, { locale: "pt-BR" }));
+      } else {
+        const scriptSdk = document.createElement("script");
+        scriptSdk.src = "https://sdk.mercadopago.com/js/v2";
+        scriptSdk.async = true;
+        scriptSdk.onload = () => {
           setSdkLoaded(true);
-          resolve();
-        } else {
-          const scriptSdk = document.createElement("script");
-          scriptSdk.src = "https://sdk.mercadopago.com/js/v2";
-          scriptSdk.async = true;
-          scriptSdk.onload = () => {
-            console.log("SDK do Mercado Pago carregado com sucesso");
-            setSdkLoaded(true);
-            resolve();
-          };
-          scriptSdk.onerror = (error) => reject(error);
-          document.body.appendChild(scriptSdk);
-        }
-      });
+          setMpInstance(new window.MercadoPago(publicKey, { locale: "pt-BR" }));
+        };
+        scriptSdk.onerror = () => console.error("Erro ao carregar o SDK.");
+        document.body.appendChild(scriptSdk);
+      }
     };
 
-    loadMercadoPagoSdk()
-      .then(() => {
-        if (window.MercadoPago) {
-          mpInstance = new window.MercadoPago(publicKey, { locale: "pt-BR" });
-          initializeDeviceId();
-        } else {
-          console.error(
-            "Erro: MercadoPago não está disponível após o carregamento do SDK."
-          );
-        }
-      })
-      .catch((error) => console.error("Erro ao carregar o SDK:", error));
+    loadMercadoPagoSdk();
   }, [publicKey]);
 
-  const initializeDeviceId = async () => {
-    const capturedDeviceId = await new Promise<string>((resolve) => {
-      const interval = setInterval(() => {
-        const device = window.MP_DEVICE_SESSION_ID;
-        if (device) {
-          clearInterval(interval);
-          resolve(device);
-        }
-      }, 100);
-    });
-    setDeviceId(capturedDeviceId);
-    console.log("Device ID gerado:", capturedDeviceId);
-  };
-
   useEffect(() => {
-    if (sdkLoaded && selectedPaymentMethod === "card" && publicKey) {
-      if (mpInstance) {
-        console.log("Configurando o cardForm...");
-        initializeCardForm(mpInstance);
-      } else {
-        console.warn("Instância do MercadoPago não está definida.");
-      }
+    if (
+      sdkLoaded &&
+      mpInstance &&
+      selectedPaymentMethod === "card" &&
+      formRef.current
+    ) {
+      initializeCardForm();
     }
-  }, [sdkLoaded, selectedPaymentMethod, publicKey, checkoutData.amount]);
+  }, [sdkLoaded, mpInstance, selectedPaymentMethod, formRef.current]);
 
-  const initializeCardForm = (mp: any) => {
-    mp.cardForm({
-      amount: String(checkoutData.amount > 1 ? checkoutData.amount : 1),
-      form: {
-        id: "form-checkout",
-        cardNumber: {
-          id: "form-checkout__cardNumber",
-          placeholder: "Número do cartão",
+  const initializeCardForm = () => {
+    if (mpInstance && formRef.current) {
+      const cardForm = mpInstance.cardForm({
+        amount: String(checkoutData.amount || 100.5),
+        iframe: true,
+        form: {
+          id: "form-checkout",
+          cardNumber: {
+            id: "form-checkout__cardNumber",
+            placeholder: "Número do cartão",
+          },
+          expirationDate: {
+            id: "form-checkout__expirationDate",
+            placeholder: "MM/YY",
+          },
+          securityCode: {
+            id: "form-checkout__securityCode",
+            placeholder: "Código de segurança",
+          },
+          cardholderName: {
+            id: "form-checkout__cardholderName",
+            placeholder: "Nome do titular",
+          },
+          issuer: {
+            id: "form-checkout__issuer",
+            placeholder: "Banco emissor",
+          },
+          installments: {
+            id: "form-checkout__installments",
+            placeholder: "Número de parcelas",
+          },
+          identificationType: {
+            id: "form-checkout__identificationType",
+            placeholder: "Tipo de documento (ex: CPF)",
+          },
+          identificationNumber: {
+            id: "form-checkout__identificationNumber",
+            placeholder: "Número do documento",
+          },
+          cardholderEmail: {
+            id: "form-checkout__cardholderEmail",
+            placeholder: "E-mail do titular",
+          },
         },
-        expirationDate: {
-          id: "form-checkout__expirationDate",
-          placeholder: "MM/YY",
+        callbacks: {
+          onFormMounted: (error: any) => {
+            if (error)
+              return console.warn("Form Mounted handling error: ", error);
+            console.log("Form mounted");
+            setIsMpReady(true);
+          },
+          onSubmit: handleCardSubmit,
+          onFetching: (resource: any) => {
+            console.log("Fetching resource: ", resource);
+            const progressBar = document.querySelector(".progress-bar");
+            if (progressBar) {
+              progressBar.removeAttribute("value");
+            }
+            return () => {
+              if (progressBar) progressBar.setAttribute("value", "0");
+            };
+          },
         },
-        securityCode: {
-          id: "form-checkout__securityCode",
-          placeholder: "Código de segurança",
-        },
-        cardholderName: {
-          id: "form-checkout__cardholderName",
-          placeholder: "Titular do cartão",
-        },
-        issuer: { id: "form-checkout__issuer", placeholder: "Banco emissor" },
-        installments: {
-          id: "form-checkout__installments",
-          placeholder: "Parcelas",
-        },
-        identificationType: {
-          id: "form-checkout__identificationType",
-          placeholder: "Tipo de documento",
-        },
-        identificationNumber: {
-          id: "form-checkout__identificationNumber",
-          placeholder: "CPF",
-        },
-        cardholderEmail: {
-          id: "form-checkout__cardholderEmail",
-          placeholder: "E-mail",
-        },
-      },
-      callbacks: {
-        onFormMounted: (error: any) => {
-          if (error) console.warn("Erro ao montar o formulário: ", error);
-          else setIsMpReady(true);
-        },
-        onSubmit: handleCardSubmit,
-      },
-    });
+      });
+    }
   };
 
   const handleCardSubmit = async (event: any) => {
     event.preventDefault();
-    const formData = (window as any).MercadoPago.getCardFormData();
+    const formData = mpInstance.getCardFormData();
     const paymentData = {
       token: formData.token,
       issuer_id: formData.issuerId,
       payment_method_id: formData.paymentMethodId,
-      transaction_amount: checkoutData.amount,
-      installments: selectedInstallment,
-      description: "Compra no e-commerce",
+      transaction_amount: Number(checkoutData.amount || 100.5),
+      installments: Number(formData.installments),
+      description: "Descrição do produto",
       payer: {
         email: formData.cardholderEmail,
-        first_name: formData.cardholderName?.split(" ")[0] || "",
-        last_name: formData.cardholderName?.split(" ").slice(1).join(" ") || "",
         identification: {
-          type: formData.identificationType || "CPF",
+          type: formData.identificationType,
           number: formData.identificationNumber,
         },
       },
-      device_id: deviceId,
-      items: checkoutData.items,
-      userId: checkoutData.userId,
     };
 
     try {
-      const response = await axios.post(
-        "https://ecommerce-fagundes-13c7f6f3f0d3.herokuapp.com/payment/process_payment",
-        paymentData
-      );
-      if (response.data.status === "approved") navigate("/sucesso");
-      else alert("Pagamento pendente ou falhou. Verifique a transação.");
+      const response = await axios.post("/process_payment", paymentData);
+      response.data.status === "approved"
+        ? navigate("/sucesso")
+        : alert("Pagamento pendente ou falhou.");
     } catch (error) {
       console.error("Erro ao finalizar o pagamento:", error);
       alert("Erro ao finalizar o pagamento.");
@@ -188,7 +156,52 @@ const Checkout: React.FC = () => {
         Boleto Bancário
       </button>
       {selectedPaymentMethod === "card" && (
-        <form id="form-checkout" onSubmit={handleCardSubmit}>
+        <form id="form-checkout" ref={formRef} onSubmit={handleCardSubmit}>
+          <input
+            type="text"
+            id="form-checkout__cardNumber"
+            placeholder="Número do cartão"
+          />
+          <input
+            type="text"
+            id="form-checkout__expirationDate"
+            placeholder="Data de expiração (MM/YY)"
+          />
+          <input
+            type="text"
+            id="form-checkout__securityCode"
+            placeholder="Código de segurança"
+          />
+          <input
+            type="text"
+            id="form-checkout__cardholderName"
+            placeholder="Nome do titular"
+          />
+          <input
+            type="text"
+            id="form-checkout__issuer"
+            placeholder="Banco emissor"
+          />
+          <input
+            type="text"
+            id="form-checkout__installments"
+            placeholder="Número de parcelas"
+          />
+          <input
+            type="text"
+            id="form-checkout__identificationType"
+            placeholder="Tipo de documento (ex: CPF)"
+          />
+          <input
+            type="text"
+            id="form-checkout__identificationNumber"
+            placeholder="Número do documento"
+          />
+          <input
+            type="email"
+            id="form-checkout__cardholderEmail"
+            placeholder="E-mail do titular"
+          />
           <button
             type="submit"
             id="form-checkout__submit"
