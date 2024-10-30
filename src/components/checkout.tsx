bf7bac85ec8519f9dadb0d5b6eb780e85fd60186
Your Name <you@example.com>
2024-10-30 17:29:10 -0300

    acrescentando pix

--- src/components/checkout.tsx
+++ src/components/checkout.tsx
@@ -138,7 +138,11 @@ const Checkout: React.FC = () => {
     };
 
     try {
-      const response = await axios.post("/process_payment", paymentData);
+      const response = await axios.post(
+        "https://ecommerce-fagundes-13c7f6f3f0d3.herokuapp.com/process_payment", // Certifique-se de que o endpoint está correto
+        paymentData,
+        { headers: { "Content-Type": "application/json" } }
+      );
       response.data.status === "approved"
         ? navigate("/sucesso")
         : alert("Pagamento pendente ou falhou.");
